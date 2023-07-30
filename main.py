037c4ec70523716cd877abd52f3f968e7bb47f41
Harshita-gupta10 <91972448+Harshita-gupta10@users.noreply.github.com>
2023-07-30 15:09:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import random
+
+options = ("rock", "paper", "scissor")
+
+# to make the game run again
+running = True
+
+while running:
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors) : ")
+
+    print(f"Player : {player}")
+    print(f"Computer : {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+        
+    elif player == "rock" and computer == "scissors":
+        print("You win :)")
+        
+    elif player == "paper" and computer == "rock":
+        print("You win :)")
+        
+    elif player == "scissors" and computer == "paper":
+        print("You win :)")
+
+    else:
+        print("You loose :(")
+    
+    play = input("Play again? (1 - yes/0 - no) : ")
+    
+    if play == 0 :
+        running = False
+
+print("Thanks for playing!")
